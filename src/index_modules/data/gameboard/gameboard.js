5af5e8e4049915603491291790012e1fd459700d
jakubkanna <hello.jakubkanna@gmail.com>
2024-01-04 20:44:00 +0100

    cleanup

--- src/index_modules/data/gameboard/gameboard.js
+++ src/index_modules/data/gameboard/gameboard.js
@@ -1,12 +1,13 @@
 import { matchCoord } from "../../utilities";
 
 export default class Gameboard {
+  
   constructor() {
     this.occupied = [];
     this.empty = this.generatedBoard;
   }
 
-  get generatedBoard() {
+  static generatedBoard() {
     const board = [];
 
     for (let i = 0; i < 10; i++) {
@@ -18,19 +19,62 @@ export default class Gameboard {
     return board;
   }
 
+  static generateShipSpace(ship, coord, axis) {
+    const [startX, startY] = coord;
+    const space = [];
+
+    for (let i = 0; i < ship.length; i++) {
+      const x = (axis === 'x') ? i + startX : startX;
+      const y = (axis === 'y') ? i + startY : startY;
+
+      if (!(0 <= x <= 9 && 0 <= y <= 9)) {
+        return false;
+      }
+
+      space.push([x, y]);
+    }
+
+    return space;
+  }
+
+  static generateShipArea(ship, coord, axis) {
+    const [startX, startY] = coord;
+    const margin = [];
+
+    const loopLimitX = (axis === 'y') ? startX + 1 : startX + ship.length;
+    const loopLimitY = (axis === 'x') ? startY + 1 : startY + ship.length;
+
+    for (let i = startX - 1; i <= loopLimitX; i++) {
+      for (let j = startY - 1; j <= loopLimitY; j++) {
+        if (0 <= i <= 9 && 0 <= j <= 9) {
+          margin.push([i, j]);
+        }
+      }
+    }
+
+    return margin;
+  }
+
+  static filterOutSpace(space, area) {
+    return area.every(coord =>
+      !space.some(spaceCoord =>
+        spaceCoord[0] === coord[0] && spaceCoord[1] === coord[1]
+      )
+    );
+  }
+
   place(ship, startCoord, axis) {
-    const space = generateShipSpace(ship, startCoord, axis);
-    const area = generateShipArea(ship, startCoord, axis);
+    const space = Gameboard.generateShipSpace(ship, startCoord, axis);
+    const area = Gameboard.generateShipArea(ship, startCoord, axis);
 
     if (!space) return false;
 
     if (this.findSpace(space)) {
-      const margin = filterOutSpace(space, area); //filter out ship from area
+      const margin = Gameboard.filterOutSpace(space, area); //filter out ship from area
       this.launchShip(ship, space, margin);
 
-      // Filter out elements from 'empty' that match elements in shipArea
-      this.reduceEmptySpace(area);
-
+      this.reduceEmptySpace(area);      // Filter out elements from 'empty' that match elements in shipArea
+     
       return true;
     } else {
       return false;
@@ -62,82 +106,4 @@ export default class Gameboard {
   }
 }
 
-function generateShipSpace(ship, coord, axis) {
-  const space = [];
-  const [startX, startY] = coord;
 
-  if (axis === "x") {
-    for (let i = 0; i < ship.length; i++) {
-      const x = i + startX;
-      const y = startY;
-
-      // Check if x or y is outside the valid range (0 to 9)
-      if (x < 0 || x > 9 || y < 0 || y > 9) {
-        return false; // Return false if outside the valid range
-      }
-
-      space.push([x, y]);
-    }
-  } else if (axis === "y") {
-    for (let i = 0; i < ship.length; i++) {
-      const x = startX;
-      const y = i + startY;
-
-      // Check if x or y is outside the valid range (0 to 9)
-      if (x < 0 || x > 9 || y < 0 || y > 9) {
-        return false; // Return false if outside the valid range
-      }
-
-      space.push([x, y]);
-    }
-  } else {
-    console.error("axis must be x or y");
-  }
-
-  return space;
-}
-
-function generateShipArea(ship, coord, axis) {
-  const margin = [];
-  const [startX, startY] = coord;
-
-  if (axis === "y") {
-    for (let i = startX - 1; i <= startX + 1; i++) {
-      for (let j = startY - 1; j <= startY + ship.length; j++) {
-        // Check if i is within the valid range (0 to 9)
-        if (i >= 0 && i <= 9) {
-          // Check if j is within the valid range (0 to 9)
-          if (j >= 0 && j <= 9) {
-            margin.push([i, j]);
-          }
-        }
-      }
-    }
-  } else {
-    for (let i = startX - 1; i <= startX + ship.length; i++) {
-      for (let j = startY - 1; j <= startY + 1; j++) {
-        // Check if i is within the valid range (0 to 9)
-        if (i >= 0 && i <= 9) {
-          // Check if j is within the valid range (0 to 9)
-          if (j >= 0 && j <= 9) {
-            margin.push([i, j]);
-          }
-        }
-      }
-    }
-  }
-
-  return margin;
-}
-
-function filterOutSpace(space, area) {
-  // create a new array with elements not present in space
-  const filteredArea = area.filter(
-    (coord) =>
-      !space.some(
-        (spaceCoord) => spaceCoord[0] === coord[0] && spaceCoord[1] === coord[1]
-      )
-  );
-
-  return filteredArea;
-}
