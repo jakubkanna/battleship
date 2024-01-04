388e6f404d8dba400e3fe64b1def246339b54d9a
jakubkanna <hello.jakubkanna@gmail.com>
2024-01-04 20:47:58 +0100

    cleanup

--- src/index_modules/data/game/game.js
+++ src/index_modules/data/game/game.js
@@ -1,8 +1,8 @@
 import Player from "../player/player";
 
 export default class Game {
-  constructor(pNum) {
-    this._players = this.createPlayers(pNum);
+  constructor(playerNumber = 2) {
+    this._players = this.createPlayers(playerNumber);
     this.setPlayerNames();
 
     this.round = 1;
@@ -18,33 +18,30 @@ export default class Game {
     return this._turn;
   }
 
-  createPlayers(playerNumber = 2) {
+  createPlayers(playerNumber) {
     return Array.from({ length: playerNumber }, () => new Player());
   }
 
   setPlayerNames() {
-    this.players.forEach((player, i) => {
-      player.name = "Player" + (i + 1);
+    this._players = this._players.map((player, i) => {
+      const newPlayer = new Player();
+      newPlayer.name = "Player" + (i + 1);
+      return newPlayer;
     });
   }
 
   nextRound() {
     this.round++;
-    this.turn++;
+    this._turn = (this._turn + 1) % this._players.length;
   }
 
-  set turn(value) {
-    this._turn = value % this.players.length;
+  static areAllShipsSunk(player) {
+    return player.gameboard.occupied.every((ship) => ship.isSunk);
   }
 
   isWinner(current) {
-    for (const player of this.players) {
-      if (player !== current && areAllShipsSunk(player)) return true;
-    }
-    return false;
+    return this._players.some(
+      (player) => player !== current && Game.areAllShipsSunk(player)
+    );
   }
-}
-
-function areAllShipsSunk(player) {
-  return player.gameboard.occupied.every((ship) => ship.isSunk);
 }
